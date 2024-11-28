2e88e10947b7771c2ffac6d44ec697b7b741fc4f
Victor Augusto <vsilva.a.augusto@gmail.com>
2024-11-28 14:03:04 -0300

    Update Order.java

--- Padrões de Projeto/Order.java
+++ Padrões de Projeto/Order.java
@@ -15,27 +15,36 @@ import com.mycompany.strategy.PayStrategy;
  * to strategy object. It can be used to save order to database.
  */
 public class Order {
+    // Variável que armazena o custo total do pedido, inicializada como 0
     private int totalCost = 0;
+    // Variável que indica se o pedido está fechado, inicializada como falso
     private boolean isClosed = false;
 
+    // Método que processa o pedido utilizando uma estratégia de pagamento
     public void processOrder(PayStrategy strategy) {
+        // Coleta os detalhes do pagamento através da estratégia fornecida
         strategy.collectPaymentDetails();
-        // Here we could collect and store payment data from the strategy.
+        // Aqui poderíamos coletar e armazenar os dados de pagamento da estratégia.
     }
 
+    // Método para definir o custo total do pedido, somando o novo custo ao existente
     public void setTotalCost(int cost) {
         this.totalCost += cost;
     }
 
+    // Método que retorna o custo total do pedido
     public int getTotalCost() {
         return totalCost;
     }
 
+    // Método que verifica se o pedido está fechado
     public boolean isClosed() {
         return isClosed;
     }
 
+    // Método que define o pedido como fechado
     public void setClosed() {
         isClosed = true;
     }
 }
+
